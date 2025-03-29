0d136df082d403156ea63147f0759e228c39a079
calebwoodworth <caleb.woodworth@gmail.com>
2025-03-29 15:25:03 -0400

    Update

--- survey.js
+++ survey.js
@@ -46,4 +46,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add a new course input field dynamically
     window.addCourse = function () {
         const coursesContainer = document.getElementById("coursesContainer");
-        const newInput = document.createElement
+        const newInput = document.createElement("input");
+        newInput.type = "text";
+        newInput.name = "courses";
+        newInput.placeholder = "New Course";
+        newInput.required = true;
+        coursesContainer.insertBefore(newInput, coursesContainer.lastElementChild);
+    };
+});
